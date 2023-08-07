43d0a79732a276f0f469653cfb0d41db7eee3dd5
Saturday <zhangkairong123@qq.com>
2023-08-07 11:42:05 +0800

    feat: add components 🎨

---
+++ src/components/AccountBank/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>开户行选择</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {
+		return {};
+	},
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/BankList/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>银行账号选择</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {
+		return {};
+	},
+});
+</script>
+
+<style scoped></style>
